9639802e22677c2e450b544412c9857f27aaae65
techiabhil <techiabhil@gmail.com>
2023-06-10 18:23:55 +0530

    test this as well

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://master--gleeful-figolla-2abbff.netlify.app/',
   plugins: [react()],
 });
